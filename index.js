ff498f7a6655501158319d4128cdd9a2348bc23f
WU,CHIEN YIN <wcy881218@gmail.com>
2022-05-03 00:28:41 +0800

    try single file

--- index.js
+++ index.js
@@ -35,28 +35,95 @@ function handleEvent(event) {
     "type": "text",
     "text": msg
   }
-  //first
-  // if(msg == "拿拿資料就走"){
-  //   //傳送想要拿什麼資訊的選單
-  //   echo = getInfo()
-  // } 
-  // else if(msg == "更深入的了解我是誰"){
-  //   //傳送訊息
-  //   echo = {
-  //     "type": "sticker",
-  //     "packageId": "446",
-  //     "stickerId": "1988"
-  //   }
-  // }
-  // else{
-  //   echo = {
-  //     "type": "text",
-  //     "text": "我不知道你在說什麼QQ 但你可以輸入 \"start\" 來了解我是誰!"
-  //   }
-  //   return client.replyMessage(event.replyToken, echo);
-  // }
+  // first
+  if(msg == "拿拿資料就走"){
+    echo = {
+      "type": "flex", 
+      "altText": "幫你的flex message取一個名字",
+      "content": {
+          "type": "bubble",
+          "hero": {
+            "type": "image",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+              "type": "uri",
+              "uri": "http://linecorp.com/"
+            },
+            "url": "https://live.staticflickr.com/65535/51940460694_a624490579_b.jpg"
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "看點東西再走吧！",
+                "weight": "bold",
+                "size": "xl"
+              }
+            ]
+          },
+          "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                  "type": "uri",
+                  "label": "Website",
+                  "uri": "https://wpbag.vercel.app/"
+                }
+              },
+              {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                  "type": "uri",
+                  "label": "Github",
+                  "uri": "https://github.com/yin1218"
+                }
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Album",
+                  "uri": "https://www.flickr.com/photos/195209755@N02/albums"
+                },
+                "height": "sm",
+                "style": "link"
+              }
+            ],
+            "flex": 0
+          }
+        }
+  }
+  } 
+  else if(msg == "更深入的了解我是誰"){
+    //傳送訊息
+    echo = {
+      "type": "sticker",
+      "packageId": "446",
+      "stickerId": "1988"
+    }
+  }
+  else{
+    echo = {
+      "type": "text",
+      "text": "我不知道你在說什麼QQ 但你可以輸入 \"start\" 來了解我是誰!"
+    }
+    return client.replyMessage(event.replyToken, echo);
+  }
   return client.replyMessage(event.replyToken, echo);
 }
+
 
 
 
