19701613fa56f99cea3fc80be3f711a182a1ed12
WU,CHIEN YIN <wcy881218@gmail.com>
2022-05-03 00:45:52 +0800

    hello world

--- index.js
+++ index.js
@@ -38,87 +38,27 @@ function handleEvent(event) {
   // first
   if(msg == "拿拿資料就走"){
     echo = {
-      "type": "flex", 
-      "altText": "My related document",
-      "content": {
-        "type": "bubble", // ①
-        "body": { // ②
-          "type": "box", // ③
-          "layout": "horizontal", // ④
-          "contents": [ // ⑤
+      "type": "flex",
+      "altText": "this is a flex message",
+      "contents": {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
             {
-              "type": "text", // ⑥
-              "text": "Hello,"
+              "type": "text",
+              "text": "hello"
             },
             {
-              "type": "text", // ⑥
-              "text": "World!"
+              "type": "text",
+              "text": "world"
             }
           ]
         }
       }
-      // "content": {
-      //     "type": "bubble",
-      //     "hero": {
-      //       "type": "image",
-      //       "size": "full",
-      //       "aspectRatio": "20:13",
-      //       "aspectMode": "cover",
-      //       "url": "https://live.staticflickr.com/65535/51940460694_a624490579_b.jpg"
-      //     },
-      //     "body": {
-      //       "type": "box",
-      //       "layout": "vertical",
-      //       "contents": [
-      //         {
-      //           "type": "text",
-      //           "text": "看點東西再走吧！",
-      //           "weight": "bold",
-      //           "size": "xl"
-      //         }
-      //       ]
-      //     },
-      //     "footer": {
-      //       "type": "box",
-      //       "layout": "vertical",
-      //       "spacing": "sm",
-      //       "contents": [
-      //         {
-      //           "type": "button",
-      //           "style": "link",
-      //           "height": "sm",
-      //           "action": {
-      //             "type": "uri",
-      //             "label": "Website",
-      //             "uri": "https://wpbag.vercel.app/"
-      //           }
-      //         },
-      //         {
-      //           "type": "button",
-      //           "style": "link",
-      //           "height": "sm",
-      //           "action": {
-      //             "type": "uri",
-      //             "label": "Github",
-      //             "uri": "https://github.com/yin1218"
-      //           }
-      //         },
-      //         {
-      //           "type": "button",
-      //           "action": {
-      //             "type": "uri",
-      //             "label": "Album",
-      //             "uri": "https://www.flickr.com/photos/195209755@N02/albums"
-      //           },
-      //           "height": "sm",
-      //           "style": "link"
-      //         }
-      //       ],
-      //       "flex": 0
-      //     }
-      // }
     }
-  } 
+  }
   else if(msg == "更深入的了解我是誰"){
     //傳送訊息
     echo = {
